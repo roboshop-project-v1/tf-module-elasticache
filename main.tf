30b88e5ac61aa024a7c2385030c6308eaf641851
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-30 23:20:47 +0530

    okok

--- main.tf
+++ main.tf
@@ -16,9 +16,12 @@ resource "aws_elasticache_cluster" "example" {
   engine               = var.engine
   node_type            = var.node_type
   num_cache_nodes      = var.num_cache_nodes
-  parameter_group_name = var.parameter_group_name
+  parameter_group_name = aws_elasticache_parameter_group.ec.name
   engine_version       = var.engine_version
   port                 = var.port
+  subnet_group_name = aws_elasticache_subnet_group.ec.name
+  security_group_ids = [aws_security_group.ec.id]
+  tags = var.tags
 }
 
 
